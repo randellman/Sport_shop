9448282eb4c22384e7c01ab52a7ddc51447e2224
Lebedev-Shvarts <Lebedev-Shvarts@ukr.net>
2019-01-27 18:27:09 +0200

    changes
    some changes

--- src/libs/libs.js
+++ src/libs/libs.js
@@ -1,5 +1,6 @@
 import "./sideBarMmenu/jquery.mmenu.all.js";
 import "./SlickNav/jquery.slicknav.min.js";
 import "./jquery-ui-1.12.1.custom/jquery-ui.js";
+import "@fortawesome/fontawesome-free/js/all.js";
 import "bootstrap";
 import "./shoppingCard/jquery.mycart.js";
